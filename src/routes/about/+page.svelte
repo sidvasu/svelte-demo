c909fb25f7972e7d77db60b03b2b7522716bb303
Siddharth Vasu <siddharth.vasu.ca@gmail.com>
2024-09-26 16:55:02 -0700

    Created counter and about

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Navbar from "$lib/components/navbar.svelte"
+</script>
+<div>ABOUT</div>
+
+<Navbar />
---
+++ src/routes/counter/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    let count = 0;
+</script>
+
+<button on:click = {() => (count += 1)}> Increment </button>
+<div>
+    inc: {count}
+</div>
